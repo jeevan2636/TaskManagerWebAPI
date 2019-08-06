24184cb2b72fd48621e2a8cc66e1c21be9f16f5d
jeevan2636 <jeevan.b2@cognizant.com>
2019-08-06 03:31:54 -0700

    Commit

---
+++ TaskMan.DAL/IUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace TaskMan.DAL
+{
+    public interface IUnitOfWork
+    {
+        
+        GenericRepository<Task> TaskRepository
+        {
+            get;
+        }
+        GenericRepository<ParentTask> ParentTaskRepository
+        {
+            get;
+        }
+        GenericRepository<VIEW_TASKManger> TaskSearchRepository
+        {
+            get;
+        }
+        void Save();
+    }
+}
---
+++ TaskMan.DAL/UnitOfWork.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMan.DAL
+{
+    public class UnitOfWork : IDisposable
+    {
+        #region Private member variables...  
+
+        private TaskmangerdbEntities _context = null;
+        private GenericRepository<Task> _taskRepository;
+        private GenericRepository<ParentTask> _parentTaskRepository;
+        private GenericRepository<VIEW_TASKManger> _viewTaskSearchRepository;
+        #endregion
+
+        public UnitOfWork()
+        {
+            _context = new TaskmangerdbEntities();
+        }
+
+        #region Public Repository Creation properties...  
+
+        /// <summary>  
+        /// Get/Set Property for product repository.  
+        /// </summary>  
+        public GenericRepository<ParentTask> ParentTaskRepository
+        {
+            get
+            {
+                if (this._parentTaskRepository == null)
+                    this._parentTaskRepository = new GenericRepository<ParentTask>(_context);
+                return _parentTaskRepository;
+            }
+        }
+
+        /// <summary>  
+        /// Get/Set Property for user repository.  
+        /// </summary>  
+        public GenericRepository<Task> TaskRepository
+        {
+            get
+            {
+                if (this._taskRepository == null)
+                    this._taskRepository = new GenericRepository<Task>(_context);
+                return _taskRepository;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public GenericRepository<VIEW_TASKManger> TaskSearchRepository
+        {
+            get
+            {
+                if (this._viewTaskSearchRepository == null)
+                    this._viewTaskSearchRepository = new GenericRepository<VIEW_TASKManger>(_context);
+                return _viewTaskSearchRepository;
+            }
+        }
+
+        #endregion
+
+        #region Public member methods...  
+        /// <summary>  
+        /// Save method.  
+        /// </summary>  
+        public void Save()
+        {
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+
+                var outputLines = new List<string>();
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    outputLines.Add(string.Format("{0}: Entity of type \"{1}\" in state \"{2}\" has the following validation errors:", DateTime.Now, eve.Entry.Entity.GetType().Name, eve.Entry.State));
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        outputLines.Add(string.Format("- Property: \"{0}\", Error: \"{1}\"", ve.PropertyName, ve.ErrorMessage));
+                    }
+                }
+                System.IO.File.AppendAllLines(@"C:\errors.txt", outputLines);
+
+                throw e;
+            }
+
+        }
+
+        #endregion
+
+        #region Implementing IDiosposable...  
+
+        #region private dispose variable declaration...  
+        private bool disposed = false;
+        #endregion
+
+        /// <summary>  
+        /// Protected Virtual Dispose method  
+        /// </summary>  
+        /// <param name="disposing"></param>  
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    Debug.WriteLine("UnitOfWork is being disposed");
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        /// <summary>  
+        /// Dispose method  
+        /// </summary>  
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
