5902302b30b873d4d80c327aa96993faee7cd9a1
jeevan2636 <jeevan.b2@cognizant.com>
2019-08-06 03:36:51 -0700

    Commit

---
+++ TaskManger.Entities/TaskEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManger.Entities
+{
+    public class TaskEntity
+    {
+        public int Task_ID { get; set; }
+        public int Parent_ID { get; set; }
+        public string TaskName { get; set; }
+        public System.DateTime Start_Date { get; set; }
+        public System.DateTime End_Date { get; set; }
+        public int Priority { get; set; }
+        public string Parent_Task { get; set; }
+
+    }
+}
